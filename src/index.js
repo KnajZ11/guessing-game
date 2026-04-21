9ee0b340d1981acd3a3715b3791ec680263622e6
KnajZ11 <knajz11@gmail.com>
2026-04-21 19:32:01 +0300

    feat: implement guessing game logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = null;
+    this.max = null;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    // Вычисляем среднее число для бинарного поиска
+    return Math.round((this.min + this.max) / 2);
+  }
 
-  greater() { }
-*/
+  lower() {
+    // Если число меньше текущего предположения,
+    // сдвигаем верхнюю границу (max) на текущее предположение
+    this.max = Math.round((this.min + this.max) / 2);
+  }
+
+  greater() {
+    // Если число больше текущего предположения,
+    // сдвигаем нижнюю границу (min) на текущее предположение
+    this.min = Math.round((this.min + this.max) / 2);
+  }
 }
 
 module.exports = GuessingGame;
